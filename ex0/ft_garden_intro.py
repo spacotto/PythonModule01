b1a63e48b41e526707ad791fddc5889f2ab606e8
spacotto <spacotto@student.42lehavre.fr>
2026-02-15 23:25:06 +0100

    Update ex0

--- ex0/ft_garden_intro.py
+++ ex0/ft_garden_intro.py
@@ -8,39 +8,35 @@ def display_info(name: str, height: int, age: int) -> None:
     """
     Displays the basic information of a plant in the garden.
     """
-    print(f" {name:<13}{f'{height}cm':<13}{f'{age} days':<13}")
+    print(f" {name:<20}{f'{height}cm':<20}{f'{age} days':<20}")
 
 
 def display_header() -> None:
     """
     Displays the header of the plant registry.
     """
-    # ANSI Color codes
     white = "\033[1;97m"
     reset = "\033[0m"
 
-    # Columns titles
-    c1 = "Name"
-    c2 = "Height"
-    c3 = "Age"
+    """Columns titles"""
+    c1, c2, c3 = "Name", "Height", "Age"
 
-    # Fixed start of program print
+    """Fixed start of program print"""
     print(f"\n{white} 🌱 Garden Plant Registry 🌱{reset}\n")
-    print(f" {white}{c1:<13}{c2:<13}{c3:<13}{reset}")
-    print(" --------------------------------------------------------")
-
+    print(f" {white}{c1:<20}{c2:<20}{c3:<20}{reset}")
+    print(" " + "-" * 60)
 
 def main() -> None:
-    # Print header
+    """Print header"""
     display_header()
 
-    # Print plants data
+    """Print plants data"""
     display_info("Rose", 25, 30)
     display_info("Sunflower", 80, 45)
     display_info("Cactus", 15, 120)
 
-    # Signal the end of the program
-    print("\n --------------------------------------------------------")
+    """Signal the end of the program"""
+    print(" " + "-" * 60)
     print(" End of Program\n")
 
 
